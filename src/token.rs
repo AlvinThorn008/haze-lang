e279058ab6304f0aacf441f3d9daa9852bb6a8ca
AlvinThorn008 <dubemogwu32@gmail.com>
2022-06-28 06:50:39 +0100

    Update: encode value in `Token`

--- src/token.rs
+++ src/token.rs
@@ -44,19 +44,19 @@ pub enum Tag {
 }
 
 #[derive(Debug, Clone)]
-pub struct Token {
+pub struct Token<'s> {
     pub tag: Tag,
-    pub loc: Range<usize>,
+    pub value: &'s str,
     pub line: u32
 }
 
 
 
-impl Token {
-    pub fn new(tag: Tag, location: Range<usize>, line: u32) -> Self {
+impl<'s> Token<'s> {
+    pub fn new(tag: Tag, value: &'s str, line: u32) -> Self {
         Self {
             tag,
-            loc: location,
+            value,
             line
         }
     }
